47d07a454d660f3ff7f9cc6da150f4fd2320da26
Nataliia Cheban <ncheban@f0r5s3.codam.nl>
2021-08-28 18:28:54 +0200

    norminette ok with key.c

--- dict/key.c
+++ dict/key.c
@@ -1,27 +1,26 @@
 int	is_digit(char c)
 {
-	if(c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
 	return (0);
 }
 
-
-int set_key(char *str, int *index, char **key)
+int	set_key(char *str, int *index, char **key)
 {
-	int result;
-	int i = 0;
+	int	result;
+	int	i;
+	int	j;
+
+	i = 0;
 	result = 0;
 	while (is_digit(str[i + *index]))
 	{
 		result = (str[i + *index] * (i + 1) + result) % max;
 		i++;
 	}
-
-	int j;
 	*key = malloc(i * sizeof (char) + 1);
-	
 	j = 0;
 	while (j < i)
 	{
@@ -29,6 +28,6 @@ int set_key(char *str, int *index, char **key)
 		j++;
 	}
 	(*key)[j] = '\0';
-	*index += + i;
-	return result;
+	*index += i;
+	return (result);
 }
