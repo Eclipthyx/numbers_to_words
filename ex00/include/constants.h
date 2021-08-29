a5e020f25b6e06e82f71903cd37c9870621a75f0
YvoHu <S262802@vuw.leidenuniv.nl>
2021-08-29 08:48:52 -0700

    update

---
+++ ex00/include/constants.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANTS_H
+# define CONSTANTS_H
+
+static int const	g_max = 10000;
+static int const	g_buffer_size = 10000000;
+#endif
---
+++ ex00/srcs/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../include/read.h"
+#include "../include/rec.h"
+#include "../include/help_functions.h"
+#include "../include/constants.h"
+
+int	main(int argc, char **argv)
+{
+	struct s_node	**dict;
+	char			*string;
+	int				start;
+
+	if (argc == 2)
+		string = argv[1];
+	else if (argc == 3)
+		string = argv[2];
+	if ((argc != 2 && argc != 3) || fill_dict(argc, argv, &dict))
+	{
+		write(1, "Dict Error\n", 11);
+		return (1);
+	}
+	start = check_string(string);
+	if (start == -1)
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	rec(string + start, count_digits(string), dict);
+	write(1, "\n", 1);
+	return (0);
+}
