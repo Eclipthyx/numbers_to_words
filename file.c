94c0a2e557e0cd924e77cb5dc52520940297dc35
Iaroslava Zaitceva <izaitcev@f0r2s7.codam.nl>
2021-08-29 13:07:57 +0200

    yayteamwork

--- file.c
+++ file.c
@@ -4,16 +4,16 @@
 #include "constants.h"
 #include "rec.h"
 
-void fill_dict(int argc, char **argv, struct s_node ***dict)
+void	fill_dict(int argc, char **argv, struct s_node ***dict)
 {
 	int		fd;
 	char	*file_string;
 	int		file_size;
 	int		i;
 
-	if(argc == 2)
+	if (argc == 2)
 		fd = open("numbers.dict", O_RDONLY);
-	else if(argc == 3)
+	else if (argc == 3)
 		fd = open(argv[1], O_RDONLY);
 	else
 		write(1, "Error\n", 6);
@@ -22,15 +22,14 @@ void fill_dict(int argc, char **argv, struct s_node ***dict)
 	i = 0;
 	*dict = malloc(g_max * sizeof (struct s_node *));
 	init_dict(*dict);
-	while(i < file_size)
+	while (i < file_size)
 	{		
-		if(is_digit(file_string[i]) && (i < file_size))
+		if (is_digit(file_string[i]) && (i < file_size))
 		{
-			struct	s_node* new_node;
+			struct s_node *new_node;
 			new_node = malloc(sizeof (struct s_node));
 			process_line(file_string, &i, new_node, *dict);
 		}
 		i++;
 	}
 }
-
--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 #include "constants.h"
 
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
     struct      s_node **dict;
-    char *string;
+    char		*string;
 	
 	fill_dict(argc, argv, &dict);
 	
-	if(argc == 2)
+	if (argc == 2)
 		string = argv[1];
-	else if(argc == 3)
+	else if (argc == 3)
 		string = argv[2];
 	else
 		write(1, "Error\n", 6);
